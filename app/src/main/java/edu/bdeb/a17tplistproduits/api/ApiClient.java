74e6688386727d031c64febb695385570cb6a8d8
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-04-17 11:28:27 -0400

    refactor ApiClient to change return type of getData method from generic to String

--- app/src/main/java/edu/bdeb/a17tplistproduits/api/ApiClient.java
+++ app/src/main/java/edu/bdeb/a17tplistproduits/api/ApiClient.java
@@ -1,6 +1,5 @@
 package edu.bdeb.a17tplistproduits.api;
 
-import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -16,7 +15,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -54,7 +52,7 @@ public class ApiClient {
             this.success = false;
         }
 
-        public T getData() {
+        public String getData() {
             return data;
         }
 
