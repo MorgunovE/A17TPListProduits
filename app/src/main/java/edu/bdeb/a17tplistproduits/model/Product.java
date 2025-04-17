2906fae8cc28fe5b25dd44146770416b8ca7ab13
Morgunov Evgenii <58105584+MorgunovE@users.noreply.github.com>
2025-04-17 10:44:31 -0400

    fix LoginActivity to change login method response type from String to Boolean

--- app/src/main/java/edu/bdeb/a17tplistproduits/model/Product.java
+++ app/src/main/java/edu/bdeb/a17tplistproduits/model/Product.java
@@ -26,6 +26,10 @@ public class Product {
         this.description = description;
     }
 
+    public Product() {
+
+    }
+
     // Getters and setters
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
